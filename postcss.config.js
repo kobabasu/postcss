b05dce72074f83b0f4c4666fef3d7c6d94d941aa
kobabasu <keiji.kobayashi.web@gmail.com>
2019-08-04 20:46:20 +0900

    css-nextからpostcss-preset-envに変更

--- postcss.config.js
+++ postcss.config.js
@@ -1,34 +1,11 @@
-module.exports = (ctx) => ({
-  map: ctx.options.map,
-
-  parser: ctx.options.parser,
-
-  autoprefixer: { browsers: 'last 2 versions' },
-
-  'local-plugins': true,
-
-  plugins: [
-    require('postcss-import')({
-      path: [
-        // npmでnormalize.cssだけインストールしているため、
-        // 以下はデフォルトでは不要。node_modulesは自動で読み込まれる
-        // bower経由でインストールした場合は以下のようにする。
-        //
-        // "vendor/normalize.css/",
-        // "vendor/animate.css/",
-        // "vendor/fontawesome/css/",
-        // "vendor/material-design-icons-iconfont/dist/"
-      ]
-    }),
-
-    require('postcss-url')(),
-
-    require('postcss-for-variables')(),
-
-    require('postcss-for')(),
-
-    require('postcss-cssnext')({
-      warnForDuplicates: false
-    }),
-  ]
-})
+module.exports = {
+  plugins: {
+    'postcss-preset-env': {
+      stage: 3,
+      importFrom: ''
+    },
+    'postcss-import': {},
+    'postcss-url': {},
+    'postcss-for-variables': {}
+  }
+}

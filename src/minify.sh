a9c6d05d194f532d0dea91721debaad447fd1084
kobabasu <keiji.kobayashi.web@gmail.com>
2019-08-06 12:31:06 +0900

    gulpのタスクをscriptsに移行。gulpを削除

---
+++ src/minify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# 対象ディレクトリ内のmin.css以外のファイルをminifyする
+#
+
+
+# 対象ディレクトリ
+readonly TARGET="../css"
+
+
+find $TARGET ! -type f -a -name "*.min.css" -exec basename {} .css \; |
+xargs -I{} cleancss $TARGET/{}.css -o $TARGET/{}.min.css
